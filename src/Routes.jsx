c64db4c9cc358a016dab68faa401a93a0dc5bf4a
lleonardus <leonardus.wizard@gmail.com>
2024-10-10 19:07:47 -0300

    adding tailwindcss

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,16 +2,14 @@ import products from "./products.json";
 
 export function Routes() {
   return (
-    <div style={{ fontFamily: "sans-serif" }}>
+    <div className="w-fit font-bold">
       <h3>Hello Audiophile</h3>
-      <ul>
+      <ul className="space-y-10 divide-y divide-solid divide-black *:pt-10">
         {products.map((p) => {
           return (
             <li key={p.id}>
               <h2>{p.name}</h2>
-              <p style={{ fontWeight: "bold", textTransform: "capitalize" }}>
-                {p.category}
-              </p>
+              <p className="font-bold capitalize">{p.category}</p>
               <img src={p.image.mobile} alt={p.name} />
             </li>
           );
