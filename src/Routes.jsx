40e4ce777537db7da353fca2584398c6676bf6fa
lleonardus <leonardus.wizard@gmail.com>
2024-10-10 20:58:35 -0300

    creating error page

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { AppLayout } from "./ui/AppLayout";
+import { ErrorPage } from "./ui/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    errorElement: <ErrorPage />,
     children: [],
   },
 ]);
