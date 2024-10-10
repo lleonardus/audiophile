e61cc9f1a08555879202b555bd8135d042cb13b9
lleonardus <leonardus.wizard@gmail.com>
2024-10-10 19:37:19 -0300

    adding colors

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,20 +1,21 @@
-import products from "./products.json";
-
 export function Routes() {
   return (
-    <div className="w-fit font-bold">
-      <h3>Hello Audiophile</h3>
-      <ul className="space-y-10 divide-y divide-solid divide-black *:pt-10">
-        {products.map((p) => {
-          return (
-            <li key={p.id}>
-              <h2>{p.name}</h2>
-              <p className="font-bold capitalize">{p.category}</p>
-              <img src={p.image.mobile} alt={p.name} />
-            </li>
-          );
-        })}
-      </ul>
+    <div className="font-bold">
+      <div>
+        <h3 className="flex gap-[34px] text-[28px] tracking-[2px]">
+          <span className="text-orange-700">01</span>
+          <span>COLORS</span>
+        </h3>
+        <div className="mt-14 grid w-fit grid-cols-4 gap-[30px] *:grid *:h-[96px] *:w-[255px] *:place-items-center *:rounded-[8px]">
+          <div className="bg-orange-700">Orange 700</div>
+          <div className="bg-gray-900 text-white">Gray 900</div>
+          <div className="bg-gray-100">Gray 100</div>
+          <div className="bg-gray-50">Gray 50</div>
+          <div className="bg-orange-400">Orange 400</div>
+          <div className="border border-gray-900/20 bg-white">White</div>
+          <div className="bg-black text-white">Black</div>
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      black: "#000000",
+      white: "#FFFFFF",
+      orange: {
+        400: "#FBAF85",
+        700: "#D87D4A",
+      },
+      gray: {
+        50: "#FAFAFA",
+        100: "#F1F1F1",
+        900: "#101010",
+      },
+    },
     extend: {},
   },
   plugins: [],
