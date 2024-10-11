73bb7de4bd08e8332dc98ecda2fe1bbc33398f05
lleonardus <leonardus.wizard@gmail.com>
2024-10-11 20:15:03 -0300

    removing black background from logo

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export function Logo() {
   return (
-    <Link to="/" className="inline-block h-[25px] w-[143px] bg-black">
+    <Link to="/" className="inline-block h-[25px] w-[143px]">
       <img src="/assets/shared/desktop/logo.svg" alt="Audiophile logo" />
     </Link>
   );
