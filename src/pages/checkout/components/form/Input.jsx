6cc3ce1bb9504d1cd6829ffcf636bd7bacb78412
lleonardus <leonardus.wizard@gmail.com>
2024-10-21 19:43:03 -0300

    increasing border width to 2px when input is invalid

--- src/pages/checkout/components/form/Input.jsx
+++ src/pages/checkout/components/form/Input.jsx
@@ -4,7 +4,7 @@ export function Input({ id, type = "text", isInvalid, placeholder, register }) {
       id={id}
       type={type}
       placeholder={placeholder}
-      className={`${isInvalid ? "border-red" : "hover:border-orange-700 focus:border-orange-700"} w-full cursor-pointer rounded-lg border border-black/40 px-6 py-[19px] text-[0.8750rem] font-bold tracking-[-0.015625rem] caret-orange-700 placeholder:text-black/40 focus:outline-none`}
+      className={`${isInvalid ? "border-[2px] border-red" : "hover:border-orange-700 focus:border-orange-700"} w-full cursor-pointer rounded-lg border border-black/40 px-6 py-[19px] text-[0.8750rem] font-bold tracking-[-0.015625rem] caret-orange-700 placeholder:text-black/40 focus:outline-none`}
       {...register}
     />
   );
