9b1b8609945482a8faa3bcc1f476594a24debc5a
lleonardus <leonardus.wizard@gmail.com>
2024-10-22 18:59:51 -0300

    creating logic to close modal and redirect the user

--- src/pages/checkout/components/SuccessMessageModal.jsx
+++ src/pages/checkout/components/SuccessMessageModal.jsx
@@ -1,9 +1,42 @@
+import { useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 import { useCart } from "../../../contexts/CartContext";
 import { ButtonLink } from "../../../ui/ButtonLink";
 
-export function SuccessMessageModal({ modalContentRef }) {
-  const { cartItems, total } = useCart();
+export function SuccessMessageModal() {
+  const { cartItems, total, removeAll } = useCart();
   const item = cartItems[0];
+
+  const modalContentRef = useRef(null);
+  const navigate = useNavigate("/");
+
+  useEffect(
+    function () {
+      function handleClick(e) {
+        const isInsideBlackArea =
+          modalContentRef.current?.parentElement.contains(e.target);
+        const isInsideWhiteArea = modalContentRef.current?.contains(e.target);
+        const isLink = e.target.tagName === "A";
+
+        if (
+          (!isInsideBlackArea && !isLink) ||
+          (isInsideBlackArea && !isInsideWhiteArea)
+        ) {
+          navigate("/");
+          removeAll();
+        } else if (
+          (!isInsideWhiteArea && isLink) ||
+          e.target.tagName === "svg"
+        ) {
+          removeAll();
+        }
+      }
+
+      window.addEventListener("mousedown", handleClick);
+      return () => window.removeEventListener("mousedown", handleClick);
+    },
+    [navigate, removeAll],
+  );
 
   return (
     <div className="absolute inset-0 z-[99999] h-full min-h-screen translate-y-[90px] bg-black/50 px-6">
@@ -25,20 +58,20 @@ export function SuccessMessageModal({ modalContentRef }) {
           <div className="bg-gray-100 pb-3 pl-4 pr-6 pt-6 sm:p-6 sm:pb-0">
             <div className="flex gap-4">
               <img
-                src={`/assets/cart/image-${item.slug}.jpg`}
-                alt={`${item.name} thubmnail`}
+                src={`/assets/cart/image-${item?.slug}.jpg`}
+                alt={`${item?.name} thubmnail`}
                 className="aspect-square h-full w-16 rounded-lg"
               />
               <div>
                 <h4 className="w-[100px] overflow-hidden text-ellipsis whitespace-nowrap text-base font-bold uppercase tracking-[0px]">
-                  {item.reducedName}
+                  {item?.reducedName}
                 </h4>
                 <p className="text-[0.875rem] font-bold leading-[25px] tracking-[0px] text-black/50">
-                  ${item.price.toLocaleString("en-US")}
+                  ${item?.price.toLocaleString("en-US")}
                 </p>
               </div>
               <p className="ml-auto text-base font-bold text-black/50">
-                x{item.quantity}
+                x{item?.quantity}
               </p>
             </div>
             {cartItems.length > 1 && (
@@ -56,7 +89,7 @@ export function SuccessMessageModal({ modalContentRef }) {
             </span>
           </div>
         </div>
-        <ButtonLink to="/" color="orange" width="100%">
+        <ButtonLink onClick={removeAll} to="/" color="orange" width="100%">
           Back to Home
         </ButtonLink>
       </div>
